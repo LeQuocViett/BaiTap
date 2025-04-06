220aab128c4eb436ac501c3fc83164d8f47426b6
LeQuocViett <vietlq.24itb@vku.udn.vn>
2025-04-06 20:29:06 +0700

    Add files via upload

---
+++ BanhMy/Consumer.java
@@ -0,0 +1,20 @@
+package BanhMy;
+
+class Consumer extends Thread {
+    Store store;
+
+    public Consumer(Store store) {
+        this.store = store;
+    }
+
+    public void run() {
+        while (true) {
+            store.get();
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
